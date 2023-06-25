e42d2dcad225055839376e7c07b54d2bb6c7d16d
Mohamedhany99 <Mohamed.hany182@outlook.com>
2023-06-25 17:51:23 +0300

    modifications in the model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from uuid import uuid4
 
 
 class Account(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=100, null=True, blank=True)
     balance = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def __str__(self) -> str:
+        return self.name
